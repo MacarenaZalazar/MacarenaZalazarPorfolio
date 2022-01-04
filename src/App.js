84a32276f8198bb8f8481b7fcdbd601bfad467b0
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2022-01-04 20:36:01 -0300

    image

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Landing from './containers/Landing/Landing';
 import Technologies from './containers/Technologies/Technologies';
 import Contact from './containers/Contact/Contact';
 import About from './containers/About/About';
+import image from './utils/imgs/aboutImg.jpg'
 
 function App() {
   return (
-      <AppDiv>
+      <AppDiv img={image}>
       <Landing/>
       <About/>
       <Technologies/>
